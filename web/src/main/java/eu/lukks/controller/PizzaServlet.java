0e087e71b05804fa2cb80b805f9fabd55914b836
lukasz.dela <lukasz.dela@interia.pl>
2018-06-08 19:30:58 +0200

    PizzaServlet correction

--- web/src/main/java/eu/lukks/controller/PizzaServlet.java
+++ web/src/main/java/eu/lukks/controller/PizzaServlet.java
@@ -2,7 +2,7 @@ package eu.lukks.controller;
 
 import java.io.IOException;
 
-@javax.servlet.annotation.WebServlet(name = "PizzaServlet")
+@javax.servlet.annotation.WebServlet("/PizzaServlet")
 public class PizzaServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
