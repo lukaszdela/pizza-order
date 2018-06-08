9f8b14c214a419702de74766676032b37f4a3066
lukasz.dela <lukasz.dela@interia.pl>
2018-06-08 19:35:34 +0200

    PizzaServlet correction - fill

--- web/src/main/java/eu/lukks/controller/PizzaServlet.java
+++ web/src/main/java/eu/lukks/controller/PizzaServlet.java
@@ -1,14 +1,35 @@
 package eu.lukks.controller;
 
+import eu.lukks.model.Pizza;
+import eu.lukks.service.IPizzaService;
+import eu.lukks.service.PizzaService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @javax.servlet.annotation.WebServlet("/PizzaServlet")
 public class PizzaServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    private IPizzaService pizzaService;
+
+    public PizzaServlet() {
+        this.pizzaService = new PizzaService();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String pizzaId = request.getParameter("pizzaId");
+        response.sendRedirect("/ClientServlet?pizzaId=" + pizzaId);
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        List<Pizza> pizzas = pizzaService.listAllPizzas();
+        request.setAttribute("pizzas", pizzas);
+        request.getRequestDispatcher("index.jsp").forward(request,response);
     }
 }
